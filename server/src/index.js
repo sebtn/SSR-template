a9d6f72c5285da593974609ef9adabbab9b064aa
sebtn <sdebedout@gmail.com>
2017-10-19 15:10:55 -0400

    Webpack server side is bundling

--- server/src/index.js
+++ server/src/index.js
@@ -1,8 +1,15 @@
 const express = require('express')
+const react = require('react')
+const renderToString = require('react-dom/server').renderToString
+
+const Home = require('./client/Components/Home').default
+
 const app = express()
 
 app.get( '/', (req, res) => {
-  
+  const content = renderToString(<Home />)
+
+  res.send(content)
 })
 
 app.listen( 3000, () => console.log('Port 3000 is ready for action') )
---
+++ server/webpack.server.js
@@ -0,0 +1,28 @@
+const path = require('path')
+
+module.exports = {
+  // build bundle for nodeJS
+  target: 'node',
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'build')
+  },
+  // run babel
+  module: {
+    rules: [
+      {
+        test: /\.js?$/,
+        loader: 'babel-loader',
+        exclude: /node-modules/,
+        options: {
+          presets: [
+            'react',
+            'stage-0',
+            ['env', { targets: { browsers: ['last 2 versions'] }}]
+          ]
+        }
+      }
+    ]
+  }
+}
